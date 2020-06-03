e57a9abdc13381ee24b90d4f5175904faf06f37b
mralexwest <66234805+mralexwest@users.noreply.github.com>
2020-06-03 10:16:12 -0700

    Create Justice.py

---
+++ Justice.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head/>
+    <title>
+        Fight the Power>
+        </title>
+        <head>
+            <body>
+                No Justice! No Peace!
+            </body>
+</html> 
